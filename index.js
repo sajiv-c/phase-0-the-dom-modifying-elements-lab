d4e0f407504c42c908b456ccb1186d6109650d32
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-09-21 17:59:01 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Sajiv is the champion";
+
+document.body.append(newHeader);
